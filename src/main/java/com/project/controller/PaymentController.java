ca82c875c085ee0610e1614e46fa273d36da9099
Emerson Millard <emillard.code@gmail.com>
2022-07-26 12:00:00 -0700

    Added controller class.

---
+++ src/main/java/com/project/controller/PaymentController.java
@@ -0,0 +1,21 @@
+package com.project.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payment-provider")
+public class PaymentController {
+
+    @Value("${server.port}")
+    private int port;
+
+    @GetMapping("/payNow/{price}")
+    public String payNow(@PathVariable int price) {
+        return "payment with " + price + " is successfull & service port : " + port;
+    }
+
+}
