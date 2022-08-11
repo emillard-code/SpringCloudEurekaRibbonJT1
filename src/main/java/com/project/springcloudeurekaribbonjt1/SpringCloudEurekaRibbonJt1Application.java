ab5b4acfd3e908a2970b2232a761ae4d28a616a9
Emerson Millard <emillard.code@gmail.com>
2022-08-11 05:48:56 -0700

    Initial commit

---
+++ src/main/java/com/project/springcloudeurekaribbonjt1/SpringCloudEurekaRibbonJt1Application.java
@@ -0,0 +1,13 @@
+package com.project.springcloudeurekaribbonjt1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudEurekaRibbonJt1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudEurekaRibbonJt1Application.class, args);
+    }
+
+}
